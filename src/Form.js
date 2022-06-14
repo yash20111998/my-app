ca452ae04d52c6dff8214bff7a11c59327de2a5f
yash20111998 <107267881+yash20111998@users.noreply.github.com>
2022-06-14 18:48:02 +0530

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Form extends Component {
   constructor(props) {
@@ -12,67 +13,57 @@ class Form extends Component {
     }
     
   }
-  nameHandler=(event)=>{
-    this.setState({
-      name:event.target.value
-    })
-  }
-  ageHandler=(event)=>{
-    this.setState({
-      age:event.target.value
-    })
-  }
-  tfHandler=(event)=>{
-    this.setState({
-      tf:event.target.value
-    })
-  }
-  interestHandler=(event)=>{
-    this.setState({
-      interest:event.target.value
-    })
+  changeHandler=(event)=>{
+    this.setState({[event.target.name]: event.target.value})
   }
   submitHandler=(event)=>{
-    alert(`${this.state.name} ${this.state.age} ${this.state.tf} ${this.state.interest}`)
+    // alert(`${this.state.name} ${this.state.age} ${this.state.tf} ${this.state.interest}`)
+    event.preventDefault()
+    console.log(this.state)
+    axios.post("url goes here ", this.state)
   }
   render() {
+    const { name, age, tf, interest }= this.state
     return (
+      <div>
       <form onSubmit={this.submitHandler} >
         <div>
           <lable><h2>NAME</h2></lable>
           <input
+            name='name'
             type="text"
-            value={this.state.name}
-            onChange={this.nameHandler}
+            value={name}
+            onChange={this.changeHandler}
             />
         </div>
         <div>
           <lable><h2>DATE OF BIRTH</h2></lable>
           <input
+            name='age'
             type="date"
-            value={this.state.age}
-            onChange={this.ageHandler}
+            value={age}
+            onChange={this.changeHandler}
             />
         </div>
         <div>
           <lable><h2>LIKE REACT</h2></lable>
-          <select value={this.state.tf } onChange={this.tfHandler}>
+          <select name= 'tf' value={tf } onChange={this.changeHandler}>
             <option value="true">TRUE</option>
             <option value="false">FALSE</option>
           </select>
         </div>
         <div>
           <lable><h2>WHICH OF THE FOLLOWING DO YOU LIKE-</h2></lable>
-          <select value={this.state.interest } onChange={this.interestHandler}>
+          <select name='interest' value={interest} onChange={this.changeHandler}>
             <option value="React">REACT</option>
             <option value="Angular">ANGULAR</option>
             <option value="Mongo">MONGO</option>
             <option value="NodeJS">NODE JS</option>
           </select>
         </div>
-        <button type='submit' className='btn'>SUBMIT</button>
+        <button type='submit'>SUBMIT</button>
       </form>
-      
+      </div>
     )
   }
 }
